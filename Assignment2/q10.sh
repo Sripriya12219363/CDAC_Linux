bd9ce72cf83376df9389452183467d34f3c9c7cc
Sripriya12219363 <119854017+Sripriya12219363@users.noreply.github.com>
2026-09-17 21:50:50 +0530

    Add script to display system information

---
+++ Assignment2/q10.sh
@@ -0,0 +1,12 @@
+echo "Current User:"
+whoami
+echo "Host Name:"
+hostname
+echo "Operating System:"
+uname -o
+echo "Kernel Version:"
+uname -r
+echo "Disk Space Usage:"
+df -h
+echo "Memory Usage:"
+free -h
